efd83fb59390b9e7eae320b90cbd4ecd162c8476
NobuokaYu <nobu0202yu@gmail.com>
2020-08-22 01:26:35 +0900

    first commit

---
+++ src/components/ListView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="list-view">
+    <div v-if="hasMemo">
+      <list-item
+        v-for="memo in filteredMemos"
+        :memo="memo"
+        @remove="remove">
+      </list-item>
+    </div>
+
+    <div v-else>
+      表示できるメモがありません。
+    </div>
+  </div>
+</template>
+
+<script lang="babel">
+  import ListItem from './ListItem'
+  export default{
+    props: {
+      memos: Array, //書き換え
+      count: Number, // 追加
+      sort: String, // 追加
+    },
+    computed: {
+      hasMemo() {
+        return this.filteredMemos && this.filteredMemos.length !== 0
+      },
+      filteredMemos() {
+        let memos = this.memos.concat()
+        if (this.sort) {
+          switch(this.sort) {
+            case 'latest':
+              memos.reverse()
+          }
+        }
+        if (this.count) {
+          memos = memos.splice(0, this.count)
+        }
+        return memos
+      }
+    },
+    methods: {
+      remove(id) {
+        this.$emit('remove', id)
+      }
+    },
+    components: {
+      'list-item': ListItem
+    }
+  }
+</script>
---
+++ src/page/Items.vue
@@ -0,0 +1,36 @@
+<style lang="scss">
+  .layout-items {
+    display: flex;
+    &-left {
+      flex-grow: 1;
+      overflow: scroll;
+    }
+    &-right {
+      border-left: 1px solid #bbb;
+    }
+  }
+</style>
+
+<template>
+  <div class="layout-items">
+    <list-view class="layout-items-left" :memos="memos"></list-view>
+    <router-view class="layout-items-right"></router-view>
+  </div>
+</template>
+
+<script>
+  import ListView from '../components/ListView'
+  export default{
+    props: {
+      memos: Array
+    },
+    methods: {
+      remove(id) {
+        this.$emit('remove', id)
+      }
+    },
+    components: {
+      ListView
+    }
+  }
+</script>
